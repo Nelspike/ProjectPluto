9fca45eb6616218626b33082e4ea6be280097031
Nelspike <nelspiketx@gmail.com>
2016-01-30 18:57:22 +0100

    Level Transition Prototype.

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Countdown : MonoBehaviour
+{
+
+  [SerializeField]
+  private int TimePerLevel = 30;
+
+	// Update is called once per frame
+	public void StartTimer ()
+	{
+	  StartCoroutine(TickDown());
+	}
+
+  public void StopTimer()
+  {
+    StopAllCoroutines();
+  }
+
+  private IEnumerator TickDown()
+  {
+    yield return new WaitForSeconds(TimePerLevel);
+
+    print("I just ticked {0} seconds".F(TimePerLevel));
+
+    // TODO (Nelspike): Transition to Loss Screen
+  }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour
+{
+
+  [SerializeField]
+  private GameObject ProposedOutcome;
+
+  [SerializeField]
+  private GameObject FinalOutcome;
+}
