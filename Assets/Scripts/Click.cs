d20b34c3f699565b7d0c956c2d573e62a76b7269
Gleb Ostrowski <glebos@web.de>
2016-01-30 21:13:07 +0100

    Add Level Start

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+
+  enum Category
+  {
+    Shell,
+    Surface,
+    Slurpable
+  }
+
+  [SerializeField]
+  private Category _category;
+
+  private Pedestal _pedestal;
+
+  public void OnClick()
+  {
+    if (!_pedestal)
+    {
+      var tag = _category == Category.Shell ? Tag.Shell : _category == Category.Slurpable ? Tag.Slurpable : Tag.Surface;
+      _pedestal = GameObject.FindGameObjectWithTag(tag).GetComponent<Pedestal>();
+      // GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+    _pedestal.Swape(gameObject);
+  }
+
+}
