df957e216eff63e921aea164ae5120404b6d39d0
Gleb Ostrowski <glebos@web.de>
2016-01-31 01:06:18 +0100

    I do a lot.
    Sorry, I am no longer able to formulate meaningful commits.
    This commits adds a lot:
    Animator Animations for MegaMan stage, for deity fly in and out.
    Also, it adds something which has to do with the mixing of stuff.
    Yeah, I think so.

---
+++ Assets/Scripts/SortingLayerExposed.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+
+// Component does nothing; editor script does all the magic
+[AddComponentMenu("UnityToolbag/Sorting Layer Exposed")]
+public class SortingLayerExposed : MonoBehaviour
+{
+}
+
+
