d9046a47847f5d20d95602169e664aff266c1d19
Gleb Ostrowski <glebos@web.de>
2016-01-31 11:02:00 +0100

    Add Hourglassssssssss snakesss

---
+++ Assets/Scripts/Hourglass.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Hourglass : MonoBehaviour
+{
+  private readonly int _runningHash = Animator.StringToHash("Running");
+
+  private Animator _animator;
+  // Use this for initialization
+  void Awake()
+  {
+    _animator = GetComponent<Animator>();
+
+  }
+
+  public void StartTimer()
+  {
+    _animator.SetBool(_runningHash, true);
+  }
+
+  public void StopTimer()
+  {
+    _animator.SetBool(_runningHash, false);
+  }
+
+  public void TimesUp()
+  {
+    //TODO (goost) Call LEvel manager and FUCK UP
+    print("Derp");
+  }
+
+  public void SetTimerLength(float _seconds)
+  {
+    _animator.speed = _seconds / 20f;
+  }
+
+}
